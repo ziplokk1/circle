d13cddcab3c98c34f420acd3e82e436ac90e8851
Mark <ziplokk.mark.sanders@gmail.com>
2017-07-03 10:37:15 -0500

    Add loop

--- waitformysql.sh
+++ waitformysql.sh
@@ -1,12 +1,18 @@
+#!/bin/bash
+
 function mysqlRunning() { \
   mysql -h 127.0.0.1 -u user -ppass -e "SHOW TABLES;" testdb > /dev/null 2>&1
   echo $?
 };
 printf "waiting for mysql"
-if [[ mysqlRunning == 1 ]]
-then
-  printf "."
-  sleep 1
-else
-  echo "mysql running"
-fi
+while true
+do
+    if [[ $(mysqlRunning) == 1 ]]
+    then
+      printf "."
+      sleep 1
+    else
+      echo "mysql running"
+      break
+    fi
+done
