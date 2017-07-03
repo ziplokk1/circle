7859cc216f868c3a4c6f70cb07a79a259e103d3b
Mark <ziplokk.mark.sanders@gmail.com>
2017-07-03 10:39:36 -0500

    --amend

--- waitformysql.sh
+++ waitformysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-function mysqlRunning() { \
+function mysqlRunning() {
+  # Make sure that user, pass, and testdb are stored somewhere else such as env variables.
   mysql -h 127.0.0.1 -u user -ppass -e "SHOW TABLES;" testdb > /dev/null 2>&1
   echo $?
 };
