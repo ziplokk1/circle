a1874911332206863a1be14ffb9d67f27fb94b23
Mark <ziplokk.mark.sanders@gmail.com>
2017-07-03 10:42:27 -0500

    Create bash script to wait for mysql to finish initializing

--- waitformysql.sh
+++ waitformysql.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
+MYSQL_BOOT_WAIT_TIMEOUT=30
+MYSQL_USER=user
+MYSQL_PASS=pass
+MYSQL_DB=testdb
+MYSQL_HOST=127.0.0.1
+
 function mysqlRunning() {
   # Make sure that user, pass, and testdb are stored somewhere else such as env variables.
-  mysql -h 127.0.0.1 -u user -ppass -e "SHOW TABLES;" testdb > /dev/null 2>&1
+  mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASS} -e "SHOW TABLES;" ${MYSQL_DB} > /dev/null 2>&1
   echo $?
 };
+
 printf "waiting for mysql"
-while true
+for i in $(seq 0 ${MYSQL_BOOT_WAIT_TIMEOUT})
 do
-    if [[ $(mysqlRunning) == 1 ]]
+    if [[ $(mysqlRunning) -eq 1 ]]
     then
       printf "."
+      if [[ ${i} -eq ${MYSQL_BOOT_WAIT_TIMEOUT} ]]
+      then
+        echo "mysql boot timeout"
+        exit 1
+      fi
       sleep 1
     else
       echo "mysql running"
