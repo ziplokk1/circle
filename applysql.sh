ff9e96993370d8e4be3b32ff07c92c5f92cbe00d
Mark <ziplokk.mark.sanders@gmail.com>
2017-07-03 10:48:18 -0500

    Add bash script to apply all sql scripts contained in ./sql/

---
+++ applysql.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SCRIPTS=./sql/*
+MYSQL_USER=user
+MYSQL_PASS=pass
+MYSQL_DB=testdb
+MYSQL_HOST=127.0.0.1
+
+for f in ${SCRIPTS}
+do
+  echo "Processing ${f}..."
+  mysql -h ${MYSQL_HOST} -u ${MYSQL_USER} -p${MYSQL_PASS} ${MYSQL_DB} < "${f}"
+done
